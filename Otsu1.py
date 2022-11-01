351ba2d550564a437701b5679e7050e173cc3884
himanshu <110653014+nerdlab53@users.noreply.github.com>
2022-11-02 00:07:48 +0530

    Update Otsu1.py

--- Otsu1.py
+++ Otsu1.py
@@ -15,28 +15,3 @@ while True:
         cv2.waitKey(1)
 
 cv2.destroyAllWindows()
-
-
-
-
-
-
-# cv2.cvtColor is applied over the
-# image input with applied parameters
-# to convert the image in grayscale
-# img = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-#
-# # applying Otsu thresholding
-# # as an extra flag in binary
-# # thresholding
-# ret, thresh1 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY +
-#                              cv2.THRESH_OTSU)
-#
-# # the window showing output image
-# # with the corresponding thresholding
-# # techniques applied to the input image
-# cv2.imshow('Otsu Threshold', thresh1)
-#
-# # De-allocate any associated memory usage
-# if cv2.waitKey(0) & 0xff == 32:
-#     cv2.destroyAllWindows()
